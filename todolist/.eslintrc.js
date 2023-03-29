15240a9352a1028869bb4bfa0ad71978a72984a7
LuisKlopp <fbgus333@gmail.com>
2023-03-29 16:33:09 +0900

    feat: 투두리스트 추가하기

--- todolist/.eslintrc.js
+++ todolist/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-native/no-inline-styles': 'off',
+  },
 };
--- todolist/App.tsx
+++ todolist/App.tsx
@@ -5,113 +5,72 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  NativeSyntheticEvent,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
+  TargetedEvent,
   Text,
+  TextInput,
+  TextInputChangeEventData,
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [todos, setTodos] = useState<{id: string; content: string}[]>([]);
+  const [inputs, setInputs] = useState<string>('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onChangeText = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>,
+  ): void => {
+    setInputs(e.nativeEvent.text);
+    console.log(inputs);
+  };
+
+  const addTodo = (): void => {
+    setTodos([...todos, {id: Date.now().toString(), content: inputs}]);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{backgroundColor: 'white', flex: 1}}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 20, color: 'black'}}>Todo-List</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder="할일을 입력해주세요."
+          style={styles.input}
+          onChange={onChangeText}
+          value={inputs}
+          onSubmitEditing={addTodo}
+          returnKeyType="done"
+        />
+      </View>
+      <View>
+        {todos.map(todo => (
+          <Text key={todo.id}>{todo.content}</Text>
+        ))}
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
   },
 });
 
